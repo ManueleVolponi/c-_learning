ea418bbfbda88de3fc007a6e73a26e05171cac7e
manueleV <manuele.volponi@gmail.com>
2025-09-10 10:03:21 +0200

    made it clear

--- todo.cpp
+++ todo.cpp
@@ -12,65 +12,72 @@ string toLower(const string& s) {
     return result;
 }
 
-struct {
-    vector<string> tasks;
-    string choice;
-} TaskManager;
 
-void addTask(string task) {
-    
-    if (task.empty()) {
-        cout << "Task cannot be empty";
-    } else {
-        TaskManager.tasks.push_back(task);
-        cout << "Task added!\n";
-    }
-}
+class TaskManager {
+    private:
+        vector<string> tasks;
 
-void viewTasks() {
-    cout << "\n--- Your tasks ---\n";
-    if (TaskManager.tasks.empty()) {
-        cout << "No tasks yet!\n";
-    } else {
-        for (string& t: TaskManager.tasks) {
-            cout << t << '\n';
+    public: 
+        string choice;
+
+        void addTask(string task) {
+            
+            if (task.empty()) {
+                cout << "Task cannot be empty";
+            } else {
+                tasks.push_back(task);
+                cout << "Task added!\n";
+            }
         }
-    }
-}
+        
+        void viewTasks() {
+            cout << "\n--- Your tasks ---\n";
+            if (tasks.empty()) {
+                cout << "No tasks yet!\n";
+            } else {
+                for (string& t: tasks) {
+                    cout << t << '\n';
+                }
+            }
+        }
+        
+        void deleteTask(int index) {
+            
+            if (index > 0 && index <= (int)tasks.size()) {
+                tasks.erase(tasks.begin() + index - 1);
+                cout << "Task removed!\n";
+            } else {
+                cout << "Invalid number!\n";
+            }
+        }
 
-void deleteTask(int index) {
-    
-    if (index > 0 && index <= (int)TaskManager.tasks.size()) {
-        TaskManager.tasks.erase(TaskManager.tasks.begin() + index - 1);
-        cout << "Task removed!\n";
-    } else {
-        cout << "Invalid number!\n";
-    }
-}
+        void performAction(string lowerChoice) {
+            if (choice == "1" || lowerChoice.find("add") != std::string::npos) {
+                string task;
+                cout << "Enter task: ";
+                getline(cin, task);
+        
+                addTask(task);
+        
+            } else if (choice == "2" || lowerChoice.find("view") != std::string::npos) {
+                viewTasks();
+        
+            } else if (choice == "3" || lowerChoice.find("remove") != std::string::npos || lowerChoice.find("del") != std::string::npos) {
+                int index;
+                cout << "Enter task number to remove: ";
+                cin >> index;
+        
+                deleteTask(index);
+            }
+        }
+};
+
 
 vector<string> defaultList = {"==== TODO's ====", "1 - Add todo", "2 - View List", "3 - Remove todo", "4 - Exit", "Choice: "};
 
-void performAction(auto choice) {
-    if (TaskManager.choice == "1" || lowerChoice.find("add") != std::string::npos) {
-        string task;
-        cout << "Enter task: ";
-        getline(cin, task);
-
-        addTask(task);
-
-    } else if (TaskManager.choice == "2" || lowerChoice.find("view") != std::string::npos) {
-        viewTasks();
-
-    } else if (TaskManager.choice == "3" || lowerChoice.find("remove") != std::string::npos || lowerChoice.find("del") != std::string::npos) {
-        int index;
-        cout << "Enter task number to remove: ";
-        cin >> index;
-
-        deleteTask(index);
-    }
-}
 
 int main() {
+    TaskManager taskManager;
 
     do {
 
@@ -84,16 +91,16 @@ int main() {
             }
         }
 
-        cin >> TaskManager.choice;
+        cin >> taskManager.choice;
 
         cin.clear(); // clear leftover newline
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Be sure that cin is totally empty, so even by typing add todo, it will keep the first param as the choice and then will ask the param relative to the task
 
-        string lowerChoice = toLower(TaskManager.choice);
+        string lowerChoice = toLower(taskManager.choice);
 
-        performAction(lowerChoice);
+        taskManager.performAction(lowerChoice);
 
-    } while (TaskManager.choice != "4" && toLower(TaskManager.choice).find("exit") == std::string::npos);
+    } while (taskManager.choice != "4" && toLower(taskManager.choice).find("exit") == std::string::npos);
 
     cout << "Goodbye!\n";
     return 0;
